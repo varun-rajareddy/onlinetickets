f118d51fc33e1438b70aa0c6a35ddcfc8e20f274
varun-rajareddy <66180708+varun-rajareddy@users.noreply.github.com>
2021-05-07 21:44:14 -0400

    initial setup

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require("path");
+const cors = require('cors');
+const mysql = require('mysql');
+
+const app = express();
+app.use(cors());
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const viewsDirPath = path.join(__dirname, "client");
+app.set("view engine", "ejs");
+app.set("views", viewsDirPath);
+app.use(express.static(path.join(__dirname, "public")));
+
+// DB connection
+const dbConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Theja@0912',
+  database : 'my_shows_db',
+  port: 3306
+});
+
+dbConnection.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected!");
+});
+
+// App Routes
+app.get('/', (request, response) => {
+  response.render('home');
+})
+
+app.listen(process.env.port || 3000);
+console.log('Web Server is listening at port '+ (process.env.port || 3000));
---
+++ setup.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE my_shows_db;
+USE my_shows_db;
